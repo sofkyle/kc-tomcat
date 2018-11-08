efe2b90fecc8cc8f46dd673485f4c8f2eb197381
glorychou <fightzyf1992@hotmail.com>
2018-11-09 00:43:07 +0800

    Servlet Http Server: Finish HttpServlet2

--- src/main/java/per/kc/tomcat/Main.java
+++ src/main/java/per/kc/tomcat/Main.java
@@ -1,9 +1,10 @@
 package per.kc.tomcat;
 
 import per.kc.tomcat.servlet.HttpServer1;
+import per.kc.tomcat.servlet.HttpServer2;
 
 public class Main {
     public static void main(String[] args) {
-        HttpServer1.start(args);
+        HttpServer2.start(args);
     }
 }
--- src/main/java/per/kc/tomcat/servlet/ServletProcessor2.java
+++ src/main/java/per/kc/tomcat/servlet/ServletProcessor2.java
@@ -10,50 +10,48 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 public class ServletProcessor2 {
+    public void process(Request request, Response response) {
+        String uri = request.getUri();
+        String servletName = uri.substring(uri.lastIndexOf("/") + 1);
+        URLClassLoader loader = null;
 
-  public void process(Request request, Response response) {
+        try {
+            // create a URLClassLoader
+            URL[] urls = new URL[1];
+            URLStreamHandler streamHandler = null;
+            File classPath = new File(Constants.WEB_ROOT);
+            // the forming of repository is taken from the createClassLoader method in
+            // org.apache.catalina.startup.ClassLoaderFactory
+            String repository = (new URL("file", null, classPath.getCanonicalPath() + File.separator)).toString();
+            // the code for forming the URL is taken from the addRepository method in
+            // org.apache.catalina.loader.StandardClassLoader class.
+            urls[0] = new URL(null, repository, streamHandler);
+            loader = new URLClassLoader(urls);
+        }
+        catch (IOException e) {
+            System.out.println(e.toString() );
+        }
+        Class myClass = null;
+        try {
+            myClass = loader.loadClass(servletName);
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
 
-    String uri = request.getUri();
-    String servletName = uri.substring(uri.lastIndexOf("/") + 1);
-    URLClassLoader loader = null;
-
-    try {
-      // create a URLClassLoader
-      URL[] urls = new URL[1];
-      URLStreamHandler streamHandler = null;
-      File classPath = new File(Constants.WEB_ROOT);
-      // the forming of repository is taken from the createClassLoader method in
-      // org.apache.catalina.startup.ClassLoaderFactory
-      String repository = (new URL("file", null, classPath.getCanonicalPath() + File.separator)).toString() ;
-      // the code for forming the URL is taken from the addRepository method in
-      // org.apache.catalina.loader.StandardClassLoader class.
-      urls[0] = new URL(null, repository, streamHandler);
-      loader = new URLClassLoader(urls);
+        Servlet servlet = null;
+        RequestFacade requestFacade = new RequestFacade(request);
+        ResponseFacade responseFacade = new ResponseFacade(response);
+        try {
+            servlet = (Servlet) myClass.newInstance();
+            responseFacade.getWriter().println("HTTP/1.1 200 OK\r\n");
+            servlet.service((ServletRequest) requestFacade, (ServletResponse) responseFacade);
+        }
+        catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        catch (Throwable e) {
+            System.out.println(e.toString());
+        }
     }
-    catch (IOException e) {
-      System.out.println(e.toString() );
-    }
-    Class myClass = null;
-    try {
-      myClass = loader.loadClass(servletName);
-    }
-    catch (ClassNotFoundException e) {
-      System.out.println(e.toString());
-    }
-
-    Servlet servlet = null;
-    RequestFacade requestFacade = new RequestFacade(request);
-    ResponseFacade responseFacade = new ResponseFacade(response);
-    try {
-      servlet = (Servlet) myClass.newInstance();
-      servlet.service((ServletRequest) requestFacade, (ServletResponse) responseFacade);
-    }
-    catch (Exception e) {
-      System.out.println(e.toString());
-    }
-    catch (Throwable e) {
-      System.out.println(e.toString());
-    }
-
-  }
 }
