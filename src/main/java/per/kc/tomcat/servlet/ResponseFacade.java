a4fa81637b5cc3619f4e45afec9aa00527ec1965
glorychou <fightzyf1992@hotmail.com>
2018-11-08 23:58:44 +0800

    Servlet Http Server: Adjusting ResponseFacade Code

--- src/main/java/per/kc/tomcat/servlet/ResponseFacade.java
+++ src/main/java/per/kc/tomcat/servlet/ResponseFacade.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.ServletOutputStream;
 
 public class ResponseFacade implements ServletResponse {
+
     private ServletResponse response;
 
     public ResponseFacade(Response response) {
@@ -15,81 +16,81 @@ public class ResponseFacade implements ServletResponse {
 
     @Override
     public String getCharacterEncoding() {
-        return null;
+        return response.getCharacterEncoding();
     }
 
     @Override
     public String getContentType() {
-        return null;
+        return response.getContentType();
     }
 
     @Override
     public ServletOutputStream getOutputStream() throws IOException {
-        return null;
+        return response.getOutputStream();
     }
 
     @Override
     public PrintWriter getWriter() throws IOException {
-        return null;
+        return response.getWriter();
     }
 
     @Override
     public void setCharacterEncoding(String charset) {
-
+        response.setCharacterEncoding(charset);
     }
 
     @Override
     public void setContentLength(int len) {
-
+        response.setContentLength(len);
     }
 
     @Override
     public void setContentLengthLong(long len) {
-
+        response.setContentLengthLong(len);
     }
 
     @Override
     public void setContentType(String type) {
-
+        response.setContentType(type);
     }
 
     @Override
     public void setBufferSize(int size) {
-
+        response.setBufferSize(size);
     }
 
     @Override
     public int getBufferSize() {
-        return 0;
+        return response.getBufferSize();
     }
 
     @Override
     public void flushBuffer() throws IOException {
-
+        response.flushBuffer();
     }
 
     @Override
     public void resetBuffer() {
-
+        response.resetBuffer();
     }
 
     @Override
     public boolean isCommitted() {
-        return false;
+        return response.isCommitted();
     }
 
     @Override
     public void reset() {
-
+        response.reset();
     }
 
     @Override
     public void setLocale(Locale loc) {
-
+        response.setLocale(loc);
     }
 
     @Override
     public Locale getLocale() {
-        return null;
+        return response.getLocale();
     }
 }
