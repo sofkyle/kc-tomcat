bdb7d9e7398e8a34df87c68b2710da20572a0b92
glorychou <fightzyf1992@hotmail.com>
2019-02-25 00:11:05 +0800

    add comment

--- src/main/java/per/kc/tomcat/Main.java
+++ src/main/java/per/kc/tomcat/Main.java
@@ -4,6 +4,8 @@ import per.kc.tomcat.servlet.HttpServer1;
 import per.kc.tomcat.servlet.HttpServer2;
 
 public class Main {
+    // 一般网页访问：127.0.0.1:8080/index.html
+    // Servlet访问：127.0.0.1:8080/servlet/PrimitiveServlet
     public static void main(String[] args) {
         HttpServer2.start(args);
     }
